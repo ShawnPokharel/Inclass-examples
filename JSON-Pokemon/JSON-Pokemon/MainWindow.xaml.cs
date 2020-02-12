1b0e33ade10ec456b6ae6ad2b8f9dddd82e748dc
Shawn Pokharel <Shawn.Pokharel-1@ou.edu>
2020-02-12 11:43:26 -0600

    Stuck on second api adding to listbox. Not sure what to do next

---
+++ JSON-Pokemon/JSON-Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string dataSetUrl = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000";
+
+            AllPokemonAPI pokemonInformation;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(dataSetUrl).Result;
+
+                pokemonInformation = JsonConvert.DeserializeObject<AllPokemonAPI>(json);
+
+               // string json = client.DeserializeObject<Pokemon>(json);
+            }
+
+            foreach(var character in pokemonInformation.results)
+            {
+                lstPokemon.Items.Add(character.name);
+            }
+        }
+
+        public void LstCharacters_SelectionChange(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedPokemonFromList = (AllPokemonResult) lstPokemon.SelectedItem;
+
+            imgPokemon.Source = new BitmapImage(new Uri(selectedPokemonFromList.image));
+        }
+
+        private void BtnPokemon_Click(object sender, RoutedEventArgs e)
+        {
+           // LstCharacters_SelectionChange(sender, RoutedEventArgs e);
+        }
+    }
+}
---
+++ JSON-Pokemon/JSON-Pokemon/Pokemon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Pokemon
+{
+    class Pokemon
+    {
+       // public AllPokemonAPI info { get; set; }
+
+       // public List<ResultObject> results { get; set; }
+
+    }
+
+    public class AllPokemonResult
+    {
+        internal string image;
+
+        public string name { get; set; }
+    
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class AllPokemonAPI
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+        public List<AllPokemonResult> results { get; set; }
+    }
+}
